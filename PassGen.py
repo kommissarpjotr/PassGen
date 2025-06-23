a1968d2a0b0777598c55a3d0cf498cb4aae91db5
kommissar_pjotr <149622380+kommissarpjotr@users.noreply.github.com>
2025-06-23 11:56:16 +0300

    Update PassGen.py

--- PassGen.py
+++ PassGen.py
@@ -3,8 +3,15 @@
 import random
 import string
 
+# Simple and effective password without special characters 
 def Password(length=32):
     chars = string.digits + string.ascii_letters  # 0-9 + a-z + A-Z
     return ''.join(random.choice(chars) for _ in range(length))
 
-print(Password())
+# Password with Special characters, usualy more secure
+def PassPlus(length=32):
+    chars = string.digits + string.ascii_letters + string.punctuation # 0-9 + a-z + A-Z + all punctuation
+    return ''.join(random.choice(chars) for _ in range(length))
+
+print("Password:", Password()) 
+print("PassPlus:", PassPlus())
